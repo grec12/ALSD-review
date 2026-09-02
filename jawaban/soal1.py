ddde449a2366c2441b044745c5d15a04ba4f22a0
grec12 <nataliagreciaaa@gmail.com>
2026-09-02 11:54:45 +0700

    71251235

--- jawaban/soal1.py
+++ jawaban/soal1.py
@@ -1,7 +1,9 @@
 def nilai_akhir(*data):
     nama, nilai_tugas, nilai_uts, nilai_uas = data
     nilai_akhir = 0.3 * nilai_tugas + 0. * nilai_uts + 0.4 * nilai_uas
+
     print(f"| {'Nama':11} :", nama, type(nama)) 
     print(f"| {'Nilai Akhir':12}: {nilai_akhir:.2f}", type(nilai_akhir)) 
-
+    return nilai_akhir
+    
 nilai_akhir("Sanka", 100, 98, 100)
